ec5a5c188a03ef14d69ca5e2eac2f534cbd5a9ac
user <user@user.com>
2026-08-30 14:48:28 +0800

    UI5

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import solid from "vite-plugin-solid";
 export default defineConfig(async () => ({
   plugins: [solid()],
 
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        settings: "settings.html",
+      },
+    },
+  },
+
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
